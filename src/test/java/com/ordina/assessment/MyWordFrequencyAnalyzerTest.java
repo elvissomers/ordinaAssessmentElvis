07c62ed61994a0494d0e077de246b0aa2dbf2398
elvissomers <elvissomers98@gmail.com>
2023-04-06 11:40:21 +0200

    Update MyWordFrequencyAnalyzerTest.java
    Update test to use Dto in every endpoint

--- src/test/java/com/ordina/assessment/MyWordFrequencyAnalyzerTest.java
+++ src/test/java/com/ordina/assessment/MyWordFrequencyAnalyzerTest.java
@@ -15,9 +15,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines over the lake";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(2, myWordFrequencyAnalyzer.calculateHighestFrequency());
+		assertEquals(2, myWordFrequencyAnalyzer.calculateHighestFrequency(testDto));
 	}
 
 	@Test
@@ -25,9 +24,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines over the lake";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord("sun"));
+		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "sun"));
 	}
 
 	@Test
@@ -35,9 +33,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines over the lake";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord("the"));
+		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "the"));
 	}
 
 	@Test
@@ -45,9 +42,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines over the lake";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord("THE"));
+		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "THE"));
 	}
 
 	@Test
@@ -55,9 +51,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines over the lake";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord("sUn"));
+		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "sUn"));
 	}
 
 	@Test
@@ -66,10 +61,9 @@ public class MyWordFrequencyAnalyzerTest {
 		String testString = "The sun shines over the lake";
 		int wordAmount = 1;
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(1)[0].getWord());
-		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(1)[0].getFrequency());
+		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, 1)[0].getWord());
+		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, 1)[0].getFrequency());
 	}
 
 	@Test
@@ -78,16 +72,15 @@ public class MyWordFrequencyAnalyzerTest {
 		String testString = "The sun shines over the lake";
 		int wordAmount = 3;
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[0].getWord());
-		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[0].getFrequency());
+		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[0].getWord());
+		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[0].getFrequency());
 
-		assertEquals("lake",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[1].getWord());
-		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[1].getFrequency());
+		assertEquals("lake",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[1].getWord());
+		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[1].getFrequency());
 
-		assertEquals("over",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[2].getWord());
-		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[2].getFrequency());
+		assertEquals("over",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[2].getWord());
+		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[2].getFrequency());
 	}
 
 	@Test
@@ -95,9 +88,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(3, myWordFrequencyAnalyzer.calculateHighestFrequency());
+		assertEquals(3, myWordFrequencyAnalyzer.calculateHighestFrequency(testDto));
 	}
 
 	@Test
@@ -105,9 +97,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord("sun"));
+		assertEquals(1, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "sun"));
 	}
 
 	@Test
@@ -115,9 +106,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(3, myWordFrequencyAnalyzer.calculateFrequencyForWord("the"));
+		assertEquals(3, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "the"));
 	}
 
 	@Test
@@ -125,9 +115,8 @@ public class MyWordFrequencyAnalyzerTest {
 		MyWordFrequencyAnalyzer myWordFrequencyAnalyzer = new MyWordFrequencyAnalyzer();
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord("LA"));
+		assertEquals(2, myWordFrequencyAnalyzer.calculateFrequencyForWord(testDto, "LA"));
 	}
 
 	@Test
@@ -136,10 +125,9 @@ public class MyWordFrequencyAnalyzerTest {
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		int wordAmount = 1;
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals("q",myWordFrequencyAnalyzer.calculateMostFrequentNWords(1)[0].getWord());
-		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(1)[0].getFrequency());
+		assertEquals("q",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, 1)[0].getWord());
+		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, 1)[0].getFrequency());
 	}
 
 	@Test
@@ -148,20 +136,17 @@ public class MyWordFrequencyAnalyzerTest {
 		String testString = "The sun shines lala la over the the     q q q     ===!! la==ke";
 		int wordAmount = 4;
 		TextDto testDto = new TextDto(testString);
-		myWordFrequencyAnalyzer.setData(testDto);
 
-		assertEquals("q",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[0].getWord());
-		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[0].getFrequency());
+		assertEquals("q",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[0].getWord());
+		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[0].getFrequency());
 
-		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[1].getWord());
-		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[1].getFrequency());
+		assertEquals("the",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[1].getWord());
+		assertEquals(3,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[1].getFrequency());
 
-		assertEquals("la",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[2].getWord());
-		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[2].getFrequency());
+		assertEquals("la",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[2].getWord());
+		assertEquals(2,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[2].getFrequency());
 
-		assertEquals("ke",myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[3].getWord());
-		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(wordAmount)[3].getFrequency());
+		assertEquals("ke",myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[3].getWord());
+		assertEquals(1,myWordFrequencyAnalyzer.calculateMostFrequentNWords(testDto, wordAmount)[3].getFrequency());
 	}
-
-	//TEst to see if can commit
 }
